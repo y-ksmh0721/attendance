1201ac5db3b77fd9c917c0f6baf82382b377aa6d
y-ksmh0721 <kov.0nce.again@gmail.com>
2025-03-27 14:21:04 +0900

    CSS読み込ませ

--- app/Http/Controllers/CsvController.php
+++ app/Http/Controllers/CsvController.php
@@ -100,3 +100,7 @@ class CsvController extends Controller
 //     $attendance->day_of_week = Carbon::parse($attendance->date)->locale('ja')->isoFormat('ddd');
 // }
 
+cd /var/www
+rm -rf attendance
+git clone github.com/y-ksmh0721/attendance attendance
+cd attendance
