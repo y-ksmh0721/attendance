65dbec5066d54b3376a0d24d928ff499e9ff0a04
y-ksmh0721 <kov.0nce.again@gmail.com>
2025-04-13 02:31:41 +0900

    戻した

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,10 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Paginator::useBootstrap();
-        // if ($this->app->environment('production')) {
-        //     URL::forceScheme('https');
-        // }
+        Paginator::useBootstrap();
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
 
