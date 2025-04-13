2a43c50766c29b1cdab55e1610d6af13fdd5a1a9
y-ksmh0721 <kov.0nce.again@gmail.com>
2025-04-13 13:51:55 +0900

    変更

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,5 @@
-<x-guest-layout>
-    <div class="container">
-        Y'sTec出勤管理システムです。<br>
-        不正のないように提出お願いします。<br><br>
-        外注の方は「外注」にチェックを入れて<br>
-        提出してください。<br>
 
-        <a href="{{ route('login') }}">
-            <button class="login-btn">ログイン</button>
-        </a>
-    </div>
-</x-guest-layout>
-{{-- @extends('layouts.app')
+@extends('layouts.app')
 
 @section('title', 'トップページ')
 
@@ -26,5 +15,5 @@
 
 
 </div>
-@endsection --}}
+@endsection
 
