196409d91a78103810cd27159922196e1dcdf365
y-ksmh0721 <kov.0nce.again@gmail.com>
2025-04-13 13:08:20 +0900

    一旦

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,16 @@
-@extends('layouts.app')
+<x-guest-layout>
+    <div class="container">
+        Y'sTec出勤管理システムです。<br>
+        不正のないように提出お願いします。<br><br>
+        外注の方は「外注」にチェックを入れて<br>
+        提出してください。<br>
+
+        <a href="{{ route('login') }}">
+            <button class="login-btn">ログイン</button>
+        </a>
+    </div>
+</x-guest-layout>
+{{-- @extends('layouts.app')
 
 @section('title', 'トップページ')
 
@@ -14,5 +26,5 @@
 
 
 </div>
-@endsection
+@endsection --}}
 
