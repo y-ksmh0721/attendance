9d6bd64aa70f6aede411f54364df5e6cf9d19dd5
y-ksmh0721 <kov.0nce.again@gmail.com>
2025-04-13 12:48:31 +0900

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,26 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/style.css', // ← これを追加
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
 });
+
+
+
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//     ],
+// });
