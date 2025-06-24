c03c09cc57d93d129960ece28931add3f11bf6c2
y-ksmh0721 <kov.0nce.again@gmail.com>
2025-06-25 00:26:38 +0900

    update.app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,17 +7,20 @@
 
 <!DOCTYPE html>
 <html lang="ja">
-<head>
+    <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', '出勤管理')</title>
-
+    {{-- 開発用 --}}
+    {{-- @vite(['resources/css/style.css', 'resources/js/app.js']) --}}
+    {{-- 本番用 --}}
+    {{-- <link rel="stylesheet" href="{{ asset('build/assets/app-CSIfVw45.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/style-C717Te9p.css') }}">
+    <script src="{{ asset('build/assets/app-CIfNNPVJ.js') }}" defer></script> --}}
     @if (app()->environment('local'))
-        {{-- 開発用 --}}
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-        @vite(['resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    @vite(['resources/js/app.js'])
     @else
-        {{-- 本番用 --}}
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         <link rel="stylesheet" href="{{ asset('build/assets/app-CSIfVw45.css') }}">
         <link rel="stylesheet" href="{{ asset('build/assets/style-sIrnz_Ww.css') }}">
